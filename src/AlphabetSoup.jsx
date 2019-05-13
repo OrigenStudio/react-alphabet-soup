042ccd7569d935b8eeffc03dad84a852cb622469
PolGuixe <pol.guixe@gmail.com>
2019-05-13 17:07:02 +0200

    added component boilerplate

---
+++ src/AlphabetSoup.jsx
@@ -0,0 +1,19 @@
+// @flow
+import * as React from 'react';
+type Props = {
+  
+};
+type State = {
+  
+};
+class AlphabetSoup extends React.Component<Props, State>{
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+};
+
+export default AlphabetSoup;
