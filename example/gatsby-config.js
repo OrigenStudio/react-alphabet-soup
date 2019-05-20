67f7441dc61268ee87121525d5db2590935aec60
PolGuixe <pol.guixe@gmail.com>
2019-05-20 19:24:06 +0200

    updated example

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `React Alphabet Soup`,
+    description: `Create super cool text animations based on an alphabet soup concept`,
+    author: `@origenstudio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `react-alphabet-soup`,
+        short_name: `RAS`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#42f4cb`,
+        theme_color: `#42f4cb`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,18 +1,46 @@
 import React from "react"
-import AlphabetSoup from "react-alphabet-soup"
+import {AlphabetSoupWithDimensions} from "react-alphabet-soup"
+import Typography from '@material-ui/core/Typography'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="react-alphabet-soup" keywords={[`react`, `animation`, `text`, `alphabet-soup`]} />
+    <Typography variant="h4" gutterBottom>Generate an Alphabet Soup like animation with the text</Typography>
+    <Typography variant="subtitle1" gutterBottom>Hover over the text area to try it!</Typography>
+    <Typography variant="h6">Sorting: 'none' | TransitionStyle='constant'</Typography>
     <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
-      <AlphabetSoup text="NEALS YARD" fontSize="24px" sorting="costFunction" costFunctionYWeight={3} />
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="none" transitionStyle="constant"/>
+    </div>
+    <Typography variant="h6">Sorting: 'none' | TransitionStyle='progressive'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="none" transitionStyle="progressive"/>
+    </div>
+    <Typography variant="h6">Sorting: 'none' | TransitionStyle='random'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="none" transitionStyle="random"/>
+    </div>
+    <Typography variant="h6">Sorting: 'costFunction' | TransitionStyle='constant'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="costFunction" costFunctionYWeight={3} transitionStyle="constant"/>
+    </div>
+    <Typography variant="h6">Sorting: 'costFunction' | TransitionStyle='progressive'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="costFunction" costFunctionYWeight={3} transitionStyle="progressive"/>
+    </div>
+    <Typography variant="h6">Sorting: 'costFunction' | TransitionStyle='random'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="costFunction" costFunctionYWeight={3} transitionStyle="random"/>
+    </div>
+    <Typography variant="h6">Sorting: 'sortByX' | TransitionStyle='random'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="sortByX" transitionStyle="random"/>
+    </div>
+    <Typography variant="h6">Sorting: 'sortByY' | TransitionStyle='random'</Typography>
+    <div style={{ width: `100%`, height: "500px", marginBottom: `1.45rem` }}>
+      <AlphabetSoupWithDimensions text="Hello, world!" fontSize="24px" sorting="sortByY" transitionStyle="random"/>
     </div>
   </Layout>
 )
